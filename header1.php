5d219acf2f099f8f1bf0f919180460dff86ecaa7
Matan Sides <165715575+MadesX@users.noreply.github.com>
2025-05-30 15:14:05 +0300

    Update header1.php

--- header1.php
+++ header1.php
@@ -1,19 +1,12 @@
-﻿fetch('header1.html')    // Load the header dynamically
+?php session_start(); ?>
+
+fetch('header1.html')    // Load the header dynamically
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
+
+        const greeting = document.getElementById("greeting");
+        const session_name = <?php echo json_encode($_SESSION['user_name'] ?? null); ?>;
+        const name = session_name != null ? session_name : "אורח";
+        greeting.textContent = `שלום ${name}`;
     });
-
-function getUserNameFromURL() {
-    const params = new URLSearchParams(window.location.search);
-    const name = params.get("name");
-    return name ? decodeURIComponent(name) : "אורח";
-}
-
-function greetOnLoad() {
-    const greeting = document.getElementById("greeting");
-    const name = getUserNameFromURL();
-    greeting.textContent = `שלום ${name}`;
-}
-
-window.onload = greetOnLoad;
