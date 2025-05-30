cf0d67eaf046b77468f7047c17f9f4f3a65dc122
Matan Sides <165715575+MadesX@users.noreply.github.com>
2025-05-30 22:44:36 +0300

    Update BookStore_entries_mysql.sql

--- database/BookStore_entries_mysql.sql
+++ database/BookStore_entries_mysql.sql
@@ -3,12 +3,15 @@ INSERT INTO categories (category) VALUES
 ('SF'), ('F'), ('R'), ('B'), ('Hi'), ('Hr'), ('M'), ('T'), ('A'), ('K'), ('C');
 
 -- Users
+INSERT INTO `admin` (userID, userName, password) VALUES (0, 'admin', 'admin');
+
 INSERT INTO `user` (fname, lname, dateOfBirth, userName, password, gender, email, phone, country, website, favoriteNumber, favoriteColor, contactTime, profilePicture, about, rating) VALUES
-('Alice', 'Smith', '1990-04-15', 'asmith', 'Alice@2023!', 'F', 'alice26@example.com', '0123456789', 'USA', 'https://aliceblog.com', 7, '#FF5733', '09:30:00', 'alice_profile.jpg', 'Love to travel and write.', 8),
+('Alice', 'Smith', '1990-04-15', 'asmith', 'Alice@2023!', 'F', 'alice26@example.com', '0123456699', 'USA', 'https://aliceblog.com', 7, '#FF5733', '09:30:00', 'alice_profile.jpg', 'Love to travel and write.', 8),
 ('Bob', 'Johnson', '1985-11-02', 'bobbyJ', 'B0b@Secure!', 'M', 'bob.j@example.org', '0987654321', 'Canada', 'https://bobjohnson.dev', 12, '#33FF99', '14:45:00', '/images/bob_pic.png', 'Software engineer and guitarist.', 10),
 ('Clara', 'Lee', '1995-06-25', 'claralee', 'Clar@2024!', 'F', 'clara.lee@example.net', '0212345678', 'Australia', 'https://claralee.io', 5, '#4455FF', '08:15:00', 'clara.png', 'Digital artist and coffee lover.', 3),
 ('David', 'Wong', '1992-03-10', 'daveW', 'D@vidW!2025', 'M', 'david.wong@example.com', '0345678912', 'Singapore', 'https://wongblog.sg', 9, '#AA22FF', '17:00:00', 'profile/dave.jpg', 'Tech enthusiast and blogger.', 9),
-('Emma', 'Brown', '1988-08-18', 'emmbrown', 'E!mma2024$', 'F', 'emma.brown@example.org', '0456789123', 'UK', 'https://emmabrown.art', 3, '#00CCFF', '12:30:00', '/users/emma_profile.jpg', 'Painter and nature lover.', 9),
+('Emma', 'Brown', '1988-08-18', 'emmbrown', 'E!mma2024$', 'F', 'emma.brown@example.org', '0456789123', 'UK', 'https://emmabrown.art', 3, '#00CCFF', '12:30:00', '/users/emma_profile.jpg', 'Painter and nature lover.', 9);
+INSERT INTO `user` (fname, lname, dateOfBirth, userName, password, gender, email, phone) VALUES
 ('Alice', 'Smith', '1990-05-12', 'alice90', 'Pass!word1', 'F', 'alice@example.com', '0541234567'),
 ('Bob', 'Jones', '1985-11-23', 'bobby85', 'Bob@Secure2', 'M', 'bob@example.com', '0522345678'),
 ('Charlie', 'Lee', '1995-08-30', 'charlie95', 'Ch@rlie3X', 'M', 'charlie@example.com', '0509876543');
