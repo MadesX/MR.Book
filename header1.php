f0778954bad117aaf4d79ee23d74aece59538879
Matan Sides <165715575+MadesX@users.noreply.github.com>
2025-05-30 22:05:35 +0300

    Update header1.php

--- header1.php
+++ header1.php
@@ -1,4 +1,4 @@
-?php session_start(); ?>
+<?php session_start(); ?>
 
 fetch('header1.html')    // Load the header dynamically
     .then(response => response.text())
@@ -9,4 +9,12 @@ fetch('header1.html')    // Load the header dynamically
         const session_name = <?php echo json_encode($_SESSION['user_name'] ?? null); ?>;
         const name = session_name != null ? session_name : "אורח";
         greeting.textContent = `שלום ${name}`;
+
+        if (name == "Administrator") {
+            fetch('navAdmin.html')
+                .then(response2 => response2.text())
+                .then(data2 => {
+                    document.getElementById('header-placeholder').innerHTML += data2;
+            });
+        }
     });
