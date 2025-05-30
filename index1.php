dedaf93a205cdc315a44f0ab1c97c48f49a45e13
Matan Sides <165715575+MadesX@users.noreply.github.com>
2025-05-30 15:15:20 +0300

    Update index1.php

--- index1.php
+++ index1.php
@@ -19,11 +19,8 @@
 	$password = "t8gwx71y";
 	$dbname = "b16_38703978_BookStore";
 
-	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
     $conn->set_charset("utf8mb4");
-
-	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
