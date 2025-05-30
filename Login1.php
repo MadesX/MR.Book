afac6934fa6df07f0be010882c5573c11c62bb38
Matan Sides <165715575+MadesX@users.noreply.github.com>
2025-05-30 14:08:06 +0300

    Update Login1.php

--- Login1.php
+++ Login1.php
@@ -8,21 +8,25 @@ $username = "b16_38703978";
 $password = "t8gwx71y";
 $dbname = "b16_38703978_BookStore";
 
-$conn = new mysqli($servername, $username, $password, $dbname);     // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$user=$_POST["username"];
-
-$sql = "SELECT userID FROM user WHERE userName='" . $user . "' AND password='" . $_POST["password"] . "'";
+$sql = "SELECT userID, fname FROM user WHERE userName='" . $_POST["username"] . "' AND password='" . $_POST["password"] . "'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    header("Location: index1.php?name=" . urlencode($user));
+    session_start();
+    $_SESSION['logged_in'] = true;
+    $_SESSION['user_id'] = $row["userID"];
+    $_SESSION['user_name'] = $row["fname"];
+
+    header("Location: index1.php");
     exit();
 } else {
-	echo "User not found!";
+	header("Location: Login1.html");
+    exit();
 }
 
 $conn->close();
