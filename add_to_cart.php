78d13d1296317b028b121a62ca5c57d795e054bf
Ron Haba <161202792+Ron12121212@users.noreply.github.com>
2025-06-03 19:56:25 +0200

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,19 +1,20 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['cart'])) {
-    $_SESSION['cart'] = [];
-}
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['bookID'])) {
-    $bookID = $_POST['bookID'];
+    $bookID = intval($_POST['bookID']);
 
-    if (!in_array($bookID, $_SESSION['cart'])) {
-        $_SESSION['cart'][] = $bookID;
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = [];
     }
-}
 
-// חזרה לעמוד הבית עם הודעה
-header("Location: index1.php?added=true");
-exit();
+    if (isset($_SESSION['cart'][$bookID])) {
+        $_SESSION['cart'][$bookID] += 1; // אם קיים הגדל כמות
+    } else {
+        $_SESSION['cart'][$bookID] = 1; // אם לא הוסף חדש
+    }
+
+    header("Location: cart.php");
+    exit;
+}
 ?>
