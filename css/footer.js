c47a5304166e15d96736c985f9327c5d7d38e6c9
Matan Sides <165715575+MadesX@users.noreply.github.com>
2025-06-05 00:36:41 +0300

    Update and rename

--- css/footer.js
+++ css/footer.js
@@ -1,4 +1,4 @@
-fetch('footer.html')    // Load the footer dynamically
+fetch('../html/footer.html')    // Load the footer dynamically
 	.then(response => response.text())
 	.then(data => {
 		document.querySelector("footer").innerHTML = data;
